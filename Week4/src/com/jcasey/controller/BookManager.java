940b6f96916422ce2fe40d8b7970945608ce4682
John Casey <john.casey@gmail.com>
2013-08-21 12:55:58 +1200

    Initial Check in of Week 4

---
+++ Week4/src/com/jcasey/controller/BookManager.java
@@ -0,0 +1,99 @@
+package com.jcasey.controller;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.classic.Session;
+import org.hibernate.criterion.Restrictions;
+
+import com.jcasey.model.Book;
+import com.jcasey.util.HibernateUtil;
+
+public class BookManager extends HibernateUtil {
+
+	public Book update(Book book) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.saveOrUpdate(book);
+		session.getTransaction().commit();
+		return book;
+	}
+	
+	public Book add(Book book) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(book);
+		session.getTransaction().commit();
+		return book;
+	}
+	public Book delete(Long id) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Book book = (Book) session.load(Book.class, id);
+		if(null != book) {
+			session.delete(book);
+		}
+		session.getTransaction().commit();
+		return book;
+	}
+
+	public Book get(Long id) {
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		LinkedList<Book> books = new LinkedList<Book>();
+		try {
+			Criteria criteria = session.createCriteria(Book.class);
+			criteria.add(Restrictions.eq("bookId", id));
+			
+			books.addAll((List<Book>)criteria.list());
+			
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit();
+		
+		if(!books.isEmpty())
+		{
+			return books.getFirst();
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public List<Book> list(String genre, String title, String author) {
+		
+	Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+	session.beginTransaction();
+	List<Book> books = null;
+	try {
+		
+		Criteria criteria = session.createCriteria(Book.class);
+		
+		// programmatically add genre criteria to our criteria obj
+		
+		// criteria.add(Restrictions.eq("genre",genre));
+		
+		books = (List<Book>)criteria.list(); // get the list
+		
+		// do the same thing using HQL
+		
+//		Query query = session.createQuery("from Book b where b.genre = :genre");
+//		query.setString("genre", genre);
+
+	} catch (HibernateException e) {
+		e.printStackTrace();
+		session.getTransaction().rollback();
+	}
+	session.getTransaction().commit();
+	return books;
+	}
+}
---
+++ Week4/src/com/jcasey/model/Book.java
@@ -0,0 +1,68 @@
+package com.jcasey.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Book")
+public class Book implements Serializable
+{
+	private static final long serialVersionUID = -6837174276610847586L;
+	private Long bookId;
+	private String title;
+	private String author;
+	private String genre;
+	private String isbn;
+	private String blurb;
+
+	@Id
+	@GeneratedValue
+	@Column(name="book_id")
+	public Long getBookId() {
+		return bookId;
+	}
+	@Column(name="title")
+	public String getTitle() {
+		return title;
+	}
+	@Column(name="author")
+	public String getAuthor() {
+		return author;
+	}
+	@Column(name="genre")
+	public String getGenre() {
+		return genre;
+	}
+	@Column(name="isbn")
+	public String getIsbn() {
+		return isbn;
+	}
+	@Column(name="blurb")
+	public String getBlurb() {
+		return blurb;
+	}
+	
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public void setBlurb(String blurb) {
+		this.blurb = blurb;
+	}
+}
