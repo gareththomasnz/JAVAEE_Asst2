f64ac76fd25e274845b7d8fea4be2890e66bdec7
John Casey <john.casey@gmail.com>
2015-05-17 23:08:43 +1200

    Add new Spring MVC + JPA project to repo

---
+++ Spring MVC JPA/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Book (BOOK_ID,TITLE, AUTHOR, GENRE) values (1,'Gone with the wind','Margaret Mitchell', 'Classic'); 
+insert into Book (BOOK_ID,TITLE, AUTHOR, GENRE) values (2,'A Clock Work Orange','Anthony Burgess','Dystopian Novella'); 
+insert into Book (BOOK_ID,TITLE, AUTHOR, GENRE) values (3,'2001: A Space Odyssey','Arthur C Clarke','Science Fiction'); 
+insert into Book (BOOK_ID,TITLE, AUTHOR, GENRE) values (4,'Make Room! Make Room!','Harry Harrison', 'Science Fiction'); 
+insert into Book (BOOK_ID,TITLE, AUTHOR, GENRE) values (5,'The Catcher in the Rye','JD Salinger', 'General');
